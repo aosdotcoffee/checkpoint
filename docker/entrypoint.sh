5b35369e59634c7aaae2a11dc5ea5ef16f198779
utf <utf16+github@riseup.net>
2025-04-18 09:37:02 +0200

    feat: tail application logs at runtime

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+start_app() {
+    php artisan pail &
+    frankenphp php-server -r public/ --listen "$CHECKPOINT_HOST:$CHECKPOINT_PORT"
+}
+
 php artisan config:cache &&
 php artisan migrate --force &&
 php artisan app:docker-startup &&
-frankenphp php-server -r public/ --listen "$CHECKPOINT_HOST:$CHECKPOINT_PORT"
+start_app
+
