6f17bc2241a81c4e2f7840effc2ddbc8357774a7
utf <utf16+github@riseup.net>
2025-04-23 00:20:29 +0200

    perf: only dispatch remote status update jobs if there are dead remotes

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Console\Commands\DispatchUpdateRemoteStatusJobs;
+use App\Services\Remote\Remote;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(DispatchUpdateRemoteStatusJobs::class)
-    ->everyThirtySeconds();
+    ->everyThirtySeconds()
+    ->when(fn () => count(Remote::dead()) > 0);
